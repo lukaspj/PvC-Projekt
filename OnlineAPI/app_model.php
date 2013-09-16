325a69723a48c7c92036c9f82dd13c05a142befe
Lukas Joergensen <ljorgensen@winterleafentertainment.com>
2013-09-16 13:38:17 +0200

    Base PHP files

---
+++ OnlineAPI/app_model.php
@@ -0,0 +1,60 @@
+﻿<?php
+class App_model extends CI_Model {
+	public function userexists($username)
+	{
+		$CI =& get_instance();
+		$query = $CI->db->query("SELECT * FROM app_users WHERE username='$username'");
+		if($query->num_rows() > 1)
+			return - 1;
+		else
+			return $query->num_rows() == 0 ? true : false;
+	}
+	//http://howto-use-mysql-spatial-ext.blogspot.dk/
+	public function updatePosition($username, $lat, $long)
+	{
+		$CI =& get_instance();
+		if($this->userexists($username))
+		{
+			$query = $CI->db->query("UPDATE app_users SET position=GeomFromText('POINT($lat $long)') WHERE username='$username'");
+			return $CI->db->_error_number();
+		}
+		else
+		{
+			$query = $CI->db->query("INSERT INTO app_users (username, position) VALUES ('$username', GeomFromText('POINT($lat $long)'))");
+			return $CI->db->_error_number();
+		}
+	}
+
+	public function addUser($username, $password, $email)
+    {
+    	$CI =& get_instance();
+		$data = array('username' => "".$username,
+						'password' => "".$password,
+						'email' => "".$email);
+        $CI->db->insert('nn_users', $data);
+		return $CI->db->_error_number();
+	}
+	
+	public function getUser($username)
+    {
+    	$CI =& get_instance();
+		$query = $CI->db->query("SELECT * FROM nn_users WHERE username='$username'");
+	}
+	
+	public function verifyPass($username, $password, $updateFlags = true)
+	{
+		log_message("error", "MYSQL ERROR verifyPass: " . $username . "  -  " . $password);
+		$CI =& get_instance();
+		$query = $CI->db->query("SELECT * FROM nn_users WHERE username='$username' AND password='$password'");
+		if($CI->db->_error_number() != 0)
+			log_message("error", "MYSQL ERROR verifyPass: " . $CI->db->_error_message() );
+		if($CI->db->_error_number() == 0){
+			$result = $query->result();
+			$CI->session->set_userdata('flags', $result[0]->flags);
+			return TRUE;
+		}
+		else
+			return FALSE;
+	}
+}
+/* END OF FILE */
---
+++ OnlineAPI/request.php
@@ -0,0 +1,69 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class request extends CI_Controller {
+
+	public function index()
+	{
+		echo "You may not directly access this page";
+	}
+
+	public function get($action)
+	{
+		//$http_origin = $_SERVER['HTTP_ORIGIN'];
+		header("Access-Control-Allow-Origin: http://nakednerd.clan-net.dk");
+		$this->load->helper('core');
+		$data = initBigCore($this);
+		$this->load->helper('user');
+		$this->load->helper('respond');
+		//$this->load->helper('frontpage');
+		switch($action)
+		{
+			case "login_check":
+				echo resp_login_check($this, $data);
+			break;
+			case "login_user":
+				echo resp_login($this, $data);
+			break;
+			case "logout_user":
+				echo resp_logout($this, $data);
+			break;
+			case "register_user":
+				echo resp_register($this, $data);
+			break;
+			case "delete_spotlights":
+				echo resp_delete_spotlights($this, $data);
+			break;
+			case "add_spotlight":
+				echo resp_add_spotlight($this, $data);
+			break;
+			case "get_spotlight_imgs":
+				echo resp_get_spotlight_imgs($this, $data);
+			break;
+			case "get_spotlight_list":
+				echo resp_get_spotlight_list($this);
+			break;
+			case "get_infoblock_imgs":
+				echo resp_get_infoblock_imgs($this, $data);
+			break;
+			case "get_infoblock_list":
+				echo resp_get_infoblock_list($this, $data);
+			break;
+			case "add_infoblock":
+				echo resp_add_infoblock($this, $data);
+			break;
+			case "delete_infoblocks":
+				echo resp_delete_infoblock($this, $data);
+			break;
+			case "delete_file":
+				echo resp_delete_file($this, $data);
+				break;
+			// Android APP API ---
+			case "update_position":
+				echo resp_app_update_position($this, $data);
+			break;
+			case "is_username_available":
+				echo resp_app_user_exists($this, $data);
+			break;
+		}
+	}
+}
