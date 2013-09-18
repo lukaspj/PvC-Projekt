0a6836e367d855d76f922de72bcd3425abe8781d
Tue Th. Holst <shagatrog@gmail.com>
2013-09-18 13:18:36 +0200

    Added some ui feats

--- src/com/pik_ant/projectslug/Register.java
+++ src/com/pik_ant/projectslug/Register.java
@@ -1,19 +1,24 @@
 package com.pik_ant.projectslug;
 
-import com.pik_ant.projectslug.R;
-
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Register extends Activity {
 
+	private Resources resourses;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		resourses = getResources();
 	}
 
 	@Override
@@ -24,20 +29,35 @@ public class Register extends Activity {
 	}
 	
 	public void register(View view){
+		//Loading animation
+		ProgressBar loading_animation = (ProgressBar) findViewById(R.id.load_anim);
+		TextView loading_text = (TextView) findViewById(R.id.load_text);
+		loading_animation.setVisibility(0);
+		loading_text.setVisibility(0);
+		
 		//Get chosen password as strings
 		EditText usrPass_1 = (EditText) findViewById(R.id.choose_pass);
 		EditText usrPass_2 = (EditText) findViewById(R.id.repeat_pass);
 		String pass_1 = usrPass_1.getText().toString();
 		String pass_2 = usrPass_2.getText().toString();
 		
+		//Check if chosen passwords are equal and set image accordingly
+		ImageView usrCheck = (ImageView) findViewById(R.id.usr_check);
+		ImageView passCheck = (ImageView) findViewById(R.id.reap_pass_check);
+		
+		if(pass_1.equals(pass_2)){
+			passCheck.setImageDrawable(resourses.getDrawable(R.drawable.raemi_check_mark));
+			passCheck.setVisibility(0);
+		}
+		else {
+			passCheck.setImageDrawable(resourses.getDrawable(R.drawable.checkerror));
+			passCheck.setVisibility(0);
+		}
+		
 		//Get chosen username as string
 		EditText usrName = (EditText) findViewById(R.id.choose_usr);
 		String userName = usrName.getText().toString();		
 		
-		//Check if the passwords are equal are equal
-		if (pass_1.equals(pass_2)){
-			//check if username is available
-		}
 	}
 
 }
